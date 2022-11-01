e7a70b48a30069a83ec399ca2f99bfc12f8cf2c2
codeisveryeasy <prafful@airasia.com>
2022-11-01 13:18:33 +0530

    codetour -> create first component

---
+++ newreactapp/src/components/header.js
@@ -0,0 +1,12 @@
+//first custom component
+import React from 'react';
+
+function HeaderComponent() {
+    return ( 
+        <div>
+            <h1>This is header component!</h1>
+        </div>
+     );
+}
+
+export default HeaderComponent;
--- newreactapp/src/root.js
+++ newreactapp/src/root.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HeaderComponent from './components/header';
 function RootComponent() {
 
     //return JSX
     return ( 
         <div>
             <h1>Root Component</h1>
+            <HeaderComponent></HeaderComponent>
         </div>
      );
 }
