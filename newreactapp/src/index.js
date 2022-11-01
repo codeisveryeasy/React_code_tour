be4f7ea9c233a279b5a40313360d66cd4d02d0ad
codeisveryeasy <prafful@airasia.com>
2022-11-01 11:35:43 +0530

    Code tour 01 created

---
+++ newreactapp/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RootComponent from './root';
+
+let location = ReactDOM.createRoot(document.getElementById("root"))
+location.render(<RootComponent></RootComponent>)
---
+++ newreactapp/src/root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function RootComponent() {
+
+    //return JSX
+    return ( 
+        <div>
+            <h1>Root Component</h1>
+        </div>
+     );
+}
+
+export default RootComponent;
