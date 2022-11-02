98bc88752586babc8180c3a221069548a5d6540b
codeisveryeasy <prafful@airasia.com>
2022-11-02 11:49:14 +0530

    codetour 03.01

--- newreactapp/src/components/header.js
+++ newreactapp/src/components/header.js
@@ -2,9 +2,25 @@
 import React from 'react';
 
 function HeaderComponent() {
+
+    let score = 8
+
+    let spaceCraft =()=>{
+        return "Let's travel to Mars"
+    }
+
+    let launch = ()=>{
+        console.log("Rocket launched....")
+    }
+
     return ( 
         <div>
             <h1>This is header component!</h1>
+            <h2>Score: {score}</h2>
+            <h2>Message: {spaceCraft()}</h2>
+            <div>
+                <button onClick={launch}>Click  Me</button>
+            </div>
         </div>
      );
 }
